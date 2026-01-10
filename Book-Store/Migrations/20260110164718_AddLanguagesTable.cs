2efc666cc34fcc54e77e3181bc9cd6dc16ca4db1
Omar Aldabbas <omaraldabbassjo@gmail.com>
2026-01-10 19:48:57 +0300

    Fix db ~ add languages table + fix ef

---
+++ Book-Store/Migrations/20260110164718_AddLanguagesTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book_Store.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLanguagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LanguageName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
