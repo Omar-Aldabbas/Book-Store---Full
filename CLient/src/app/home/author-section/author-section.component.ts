0334f0087cc6fdde78d1ade598c4e4691dab8a21
Omar Aldabbas <omaraldabbassjo@gmail.com>
2026-01-11 22:00:04 +0300

    author section ts

--- CLient/src/app/home/author-section/author-section.component.ts
+++ CLient/src/app/home/author-section/author-section.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Author {
+  id: number;
+  name: string;
+  imageUrl: string;
+  bio: string;
+  popularBook: {
+    title: string;
+  };
+}
 
 @Component({
   selector: 'app-author-section',
   templateUrl: './author-section.component.html',
-  styleUrl: './author-section.component.css'
+  styleUrls: ['./author-section.component.css']
 })
-export class AuthorSectionComponent {
+export class AuthorSectionComponent implements OnInit {
 
+  authors: Author[] = [
+    {
+      id: 1,
+      name: 'J.K. Rowling',
+      imageUrl: '/assets/authors/jk_rowling.jpg',
+      bio: 'British author best known for the Harry Potter series.',
+      popularBook: { title: "Harry Potter and the Philosopher's Stone" }
+    },
+    {
+      id: 2,
+      name: 'George R.R. Martin',
+      imageUrl: '/assets/authors/grrm.jpg',
+      bio: 'American novelist famous for A Song of Ice and Fire series.',
+      popularBook: { title: 'A Game of Thrones' }
+    },
+    {
+      id: 3,
+      name: 'Agatha Christie',
+      imageUrl: '/assets/authors/agatha.jpg',
+      bio: 'Prolific English writer known for detective novels.',
+      popularBook: { title: 'Murder on the Orient Express' }
+    }
+  ];
+
+  currentAuthorIndex = 0;
+  author!: Author;
+
+  ngOnInit(): void {
+    this.author = this.authors[this.currentAuthorIndex];
+    this.rotateAuthors();
+  }
+
+  rotateAuthors() {
+    setInterval(() => {
+      this.currentAuthorIndex = (this.currentAuthorIndex + 1) % this.authors.length;
+      this.author = this.authors[this.currentAuthorIndex];
+    }, 5000); 
+  }
 }
