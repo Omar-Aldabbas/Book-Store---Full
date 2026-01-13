221eeabed0ec3fa714962d1ea47a2b4faf739460
Omar Aldabbas <omaraldabbassjo@gmail.com>
2026-01-13 19:37:57 +0300

    add author table + redefine relations

---
+++ Book-Store/Models/BookGenre.cs
@@ -0,0 +1,6 @@
+﻿namespace Book_Store.Models
+{
+    public class BokkGenre
+    {
+    }
+}
---
+++ Book-Store/Models/DataPrecision.cs
@@ -0,0 +1,11 @@
+﻿namespace Book_Store.Models
+{
+    public enum DatePrecision
+    {
+        Unknown = 0,
+        Year = 1,
+        YearMonth = 2,
+        FullDate = 3
+    }
+
+}
